e5fdd00fb5276e8e6bbfb4365f13b51925113edf
mohcine <mohcine.sakhi@gmail.com>
2022-02-04 12:05:36 +0100

    Choix de la structure de données et ajout des fonctionnalités 'Initialise', 'Affiche' et 'Joue'

--- Serie07/src/jeu/Jeu.java
+++ Serie07/src/jeu/Jeu.java
@@ -13,13 +13,18 @@ public class Jeu {
         int[][] grille = new int[LIGNE][COLONNE];
             
         initialise( grille );
-        grille[2][3] = ROUGE;
-        grille[3][5] = ROUGE;
-        grille[4][1] = VERT;
-        grille[3][3] = VERT;
-        
         affiche( grille );
         
+        for ( int i = 0; i < 10; i++ ) {
+            boolean valide = joue( grille, 2, ROUGE );
+            
+            if(valide) {
+                affiche(grille);
+            }else {
+                System.out.println( "Impossible d'ajouter un pion dans cette colonne !" );
+            }
+        }
+      
     }
     
     public static void initialise(int[][] grille) {
@@ -54,6 +59,23 @@ public class Jeu {
         }
         System.out.println( "==" );
         
+    }
+    
+    public static Boolean joue(int[][] grille, int colonne, int couleur) {
+        // la colonne est pleine
+        if(grille[0][colonne] != VIDE) {
+            return false;
+        }else {
+            // on cherche la ligne vide à partie du bas
+            int ligne = grille.length - 1;
+            while(grille[ligne][colonne] != VIDE) {
+                --ligne;
+            }
+            
+            //ajout du coup^
+            grille[ligne][colonne] = couleur;
+            return true;
+        }
     }
 
 }
